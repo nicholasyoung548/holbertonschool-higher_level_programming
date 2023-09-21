d5d3cf28292756cea9cfc7969d22c740d34f3da7
Nick <nicholasyoung548@yahoo.com>
2023-09-20 19:26:31 -0700

    misery

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     """Prints the result of the addition between a and b"""
 
-from add_0.py import add
+from add_0 import add
 a = 1
 b = 2
 print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
