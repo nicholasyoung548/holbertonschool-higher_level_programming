848c2fd698dcd52eaf74a473cd15aefda5a93b29
Nick <nicholasyoung548@yahoo.com>
2023-09-20 20:46:34 -0700

    Stream

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -5,10 +5,10 @@ def list_division(my_list_1, my_list_2, list_length):
         try:
             division = my_list_1[i] / my_list_2[i]
         except ZeroDivisionError:
-            print("dividing by 0")
+            print("division by 0")
             division = 0
         except IndexError:
-            print("out of Index Range")
+            print("out of range")
             division = 0
         except TypeError:
             print("wrong type")
